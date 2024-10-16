cf14cbfd282086fb94930843bf0b7af4ec39a815
thezeeshann <info.zeeshann@gmail.com>
2024-10-16 13:31:05 +0530

    feat: Added shopping list

---
+++ app/index.tsx
@@ -0,0 +1,80 @@
+import { StyleSheet, TextInput, FlatList, Text, View } from "react-native";
+import { theme } from "../theme";
+import ShoppingListItem from "../components/ShoppingListItem";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+export default function App() {
+  const [shoppingList, setShoppingList] = useState<ShoppingListItemType[]>([]);
+  const [value, setValue] = useState<string>();
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        {
+          id: new Date().toTimeString(),
+          name: value,
+        },
+        ...shoppingList,
+      ];
+
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+
+  return (
+    <FlatList
+      ListHeaderComponent={
+        <TextInput
+          value={value}
+          style={styles.textInput}
+          onChangeText={setValue}
+          placeholder="E.g Coffee"
+          onSubmitEditing={handleSubmit}
+          returnKeyType="done"
+        />
+      }
+      data={shoppingList}
+      ListEmptyComponent={
+        <View style={styles.listEmptyContainer}>
+          <Text>Your shopping list is empty</Text>
+        </View>
+      }
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      renderItem={({ item }) => <ShoppingListItem name={item.name} />}
+    ></FlatList>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colorWhite,
+    paddingTop: 12,
+  },
+  contentContainer: {
+    paddingVertical: 12,
+    paddingBottom: 24,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    fontSize: 18,
+    borderRadius: 50,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    backgroundColor: theme.colorWhite,
+  },
+  listEmptyContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 18,
+  },
+});
---
+++ components/ShoppingListItem.tsx
@@ -0,0 +1,91 @@
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { theme } from "../theme";
+
+type ShoppingListItemProp = {
+  name: string;
+  isCompleted?: boolean;
+};
+
+const ShoppingListItem = ({ name, isCompleted }: ShoppingListItemProp) => {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this?",
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Ok, deleting."),
+          style: "destructive",
+        },
+        { text: "Cancel", style: "cancel" },
+      ]
+    );
+  };
+
+  return (
+    <View
+      style={[
+        styles.itemContainer,
+        isCompleted ? styles.completedContainer : undefined,
+      ]}
+    >
+      <Text
+        style={[
+          styles.itemText,
+          isCompleted ? styles.completedText : undefined,
+        ]}
+      >
+        {name}
+      </Text>
+      <TouchableOpacity hitSlop={20} onPress={handleDelete}>
+        <AntDesign
+          name="closecircle"
+          size={24}
+          color={isCompleted ? theme.colorGrey : theme.colorRed}
+        />
+       </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    paddingVertical: 16,
+    paddingHorizontal: 8,
+    borderBottomColor: theme.colorCerulean,
+    borderBottomWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  completedContainer: {
+    backgroundColor: theme.colorLightGrey,
+    borderBottomColor: theme.colorLightGrey,
+  },
+  completedText: {
+    color: theme.colorGrey,
+    textDecorationLine: "line-through",
+    textDecorationColor: theme.colorGrey,
+  },
+  completedButton: {
+    backgroundColor: theme.colorGrey,
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  },
+});
+
+export default ShoppingListItem;
